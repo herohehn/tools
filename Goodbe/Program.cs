940747006deddfc6f8c9d441ef338b50d145473f
herohehn <59056614+herohehn@users.noreply.github.com>
2019-12-19 15:43:13 +0100

    Initial check-in

---
+++ Goodbe/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Goodbe
+{
+    static internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Goodbe";
+            Console.SetWindowSize(25, 8);
+
+            //DateTime StartupTime = DateTime.Now - new TimeSpan(0, 0, 0, 0, Environment.TickCount);
+            PrintTimes(Process.GetCurrentProcess().StartTime);
+
+            while (true)
+            {
+                Console.WriteLine("\nStartzeitkorrektur: ");
+                string newTime = Console.ReadLine();
+
+                try
+                {
+                    Console.Clear();
+                    PrintTimes(DateTime.Parse(newTime));
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+
+        private static void PrintTimes(DateTime startTime)
+        {
+            Console.WriteLine("Arbeitsbeginn: \t" + startTime.ToShortTimeString());
+            Console.WriteLine("6h Pause: \t" + startTime.AddHours(6).ToShortTimeString());
+            Console.WriteLine("Feierabend: \t" + startTime.AddHours(7).AddMinutes(36).AddMinutes(30).ToShortTimeString());
+            Console.WriteLine("9h Pause: \t" + startTime.AddHours(9).AddMinutes(30).ToShortTimeString());
+        }
+    }
+}
---
+++ Goodbee/MainWindow.xaml.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Media;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Threading;
+using Microsoft.WindowsAPICodePack.Taskbar;
+
+namespace Goodbee
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        DateTime Arbeitsbeginn;
+        DateTime SechsStundenPause { get { return Arbeitsbeginn.AddHours(6); } }
+        DateTime Feierabend { get { return Arbeitsbeginn.AddHours(7.6).AddMinutes(30); } }
+        DateTime NeunStundenPause { get { return Arbeitsbeginn.AddHours(9.5); } }
+        private bool soundHasBeenPlayed;
+        DispatcherTimer timer = new DispatcherTimer();
+
+        /// <summary>
+        /// Initial setup
+        /// </summary>
+        public MainWindow()
+        {
+            InitializeComponent();
+            Arbeitsbeginn = Process.GetCurrentProcess().StartTime;
+
+            RefreshDisplayTimes();
+
+            // Start timer for taskbar progress
+            timer.Tick += new EventHandler(UpdateNotifications);
+            timer.Interval = new TimeSpan(0, 0, 10); // Fire every (h, m, s)
+            timer.Start();
+        }
+
+        /// <summary>
+        /// Notification handling
+        /// </summary>
+        private void UpdateNotifications(object sender, EventArgs e)
+        {
+            DateTime currentTime = DateTime.Now;
+            TaskbarManager.Instance.SetProgressValue((int)(currentTime - Arbeitsbeginn).TotalMinutes, (int)(Feierabend - Arbeitsbeginn).TotalMinutes); // (currentValue, maxValue) - minValue is always 0
+
+            // Taskbar color handling
+            if (currentTime > SechsStundenPause && currentTime < SechsStundenPause.AddMinutes(30))
+            {
+                // Pause state (color change) during 6h break
+                TaskbarManager.Instance.SetProgressState(TaskbarProgressBarState.Paused);
+            }
+            else if (currentTime > Feierabend)
+            {
+                if (!soundHasBeenPlayed)
+                {
+                    // Play soundfile FEIERABEND.wav by Patrick Fuhlert
+                    SoundPlayer sound = new SoundPlayer(Properties.Resources.FEIERABEND);
+                    sound.Play();
+                    soundHasBeenPlayed = true;
+                }
+
+                // Indeterminate state (flashing) when you should call it a day
+                TaskbarManager.Instance.SetProgressState(TaskbarProgressBarState.Indeterminate);
+            }
+            else
+            {
+                // Default state
+                TaskbarManager.Instance.SetProgressState(TaskbarProgressBarState.Normal);
+            }
+        }
+
+        /// <summary>
+        /// Set startup time using the enter key
+        /// </summary>
+        private void ArbeitsbeginnTextBox_OnKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                SetArbeitsbeginn();
+            }
+        }
+
+        /// <summary>
+        /// Clear textbox when focused
+        /// </summary>
+        private void ArbeitsbeginnTextBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            ArbeitsbeginnTextBox.Clear();
+        }
+
+        /// <summary>
+        /// Set startup time when losing focus
+        /// </summary>
+        private void ArbeitsbeginnTextBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            SetArbeitsbeginn();
+        }
+
+        /// <summary>
+        /// Change startup time using the interface
+        /// </summary>
+        private void SetArbeitsbeginn()
+        {
+            if (ArbeitsbeginnTextBox.Text.Length <= 0)
+            {
+                ArbeitsbeginnTextBox.Text = Arbeitsbeginn.ToShortTimeString();
+            }
+            else
+            {
+                if (DateTime.TryParse(ArbeitsbeginnTextBox.Text, out Arbeitsbeginn))
+                {
+                    // Clear textbox background
+                    ArbeitsbeginnTextBox.ClearValue(BackgroundProperty);
+
+                    // Refresh display times
+                    RefreshDisplayTimes();
+                    timer.Start();
+                }
+                else
+                {
+                    // Error state
+                    TaskbarManager.Instance.SetProgressState(TaskbarProgressBarState.Error);
+                    ArbeitsbeginnTextBox.Background = Brushes.IndianRed;
+                    timer.Stop();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Refresh display times
+        /// </summary>
+        private void RefreshDisplayTimes()
+        {
+            ArbeitsbeginnTextBox.Text = Arbeitsbeginn.ToShortTimeString();
+            SechsStundenPauseTextBox.Text = SechsStundenPause.ToShortTimeString();
+            FeierabendTextBox.Text = Feierabend.ToShortTimeString();
+            NeunStundenPauseTextBox.Text = NeunStundenPause.ToShortTimeString();
+        }
+    }
+}
