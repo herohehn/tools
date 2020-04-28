847b4ceae6ec74a2b5e6d2a29bf0cc2a4cfa3e75
Henner Nielsen <henner.nielsen@hamburgenergie.de>
2020-04-28 15:53:49 +0200

    Added deserialization of wordlists

--- SprintingSlug/MainWindow.xaml.cs
+++ SprintingSlug/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +21,23 @@ namespace SprintingSlug
     /// </summary>
     public partial class MainWindow : Window
     {
+        List<string> adjectives;
+        List<string> animals;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            // Deserialize wordlists
+            try
+            {
+                adjectives = File.ReadAllLines(Directory.GetCurrentDirectory() + @"\Wordlists\Adjectives.txt").ToList();
+                animals = File.ReadAllLines(Directory.GetCurrentDirectory() + @"\Wordlists\Animals.txt").ToList();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
     }
 }
