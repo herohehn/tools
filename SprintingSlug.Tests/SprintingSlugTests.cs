0b5ee2438d5883e2d9bede4bcba2efe1a381cd23
Henner Nielsen <henner.nielsen@hamburgenergie.de>
2020-04-28 15:31:52 +0200

    Added some unit test ideas

--- SprintingSlug.Tests/SprintingSlugTests.cs
+++ SprintingSlug.Tests/SprintingSlugTests.cs
@@ -9,11 +9,21 @@ namespace SprintingSlug.Tests
         [TestMethod]
         public void GetAdjectives()
         {
+            // Returns a list of adjectives
+            // All trailing chars match given parameter
         }
 
         [TestMethod]
         public void GetAnimals()
         {
+            // Returns a list of animals
+            // All trailing chars match given parameter
         }
+
+        // All trailing chars are uppercase
+        // Different chars  (a, h, n, w and z)
+        // Unallowed input throws (' ', %, -, .)
+        // Weird but allowed (Ó, ô, Ô, ö, Ö, ŏ, ō, Ō, ő, ø)
+        // Uppercase and lowercase parameters
     }
 }
