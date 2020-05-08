d844c0fed68c8985f4edeac81a0e284a5c2c4535
Henner Nielsen <henner.nielsen@hamburgenergie.de>
2020-05-08 16:06:08 +0200

    Added first meaningful unit tests for GetAdjectives

--- SprintingSlug.Tests/SprintingSlugTests.cs
+++ SprintingSlug.Tests/SprintingSlugTests.cs
@@ -9,24 +9,38 @@ namespace SprintingSlug.Tests
         MainWindow sprintingSlug = new MainWindow();
 
         [TestMethod]
-        public void GetAdjective()
+        public void GetAdjective_NonEmptyString()
         {
-            // Returns an adjective
-            // Trailing char matches given parameter
-            Assert.AreEqual('a', sprintingSlug.GetAdjective('a').ToCharArray()[0]);
+            // Returns a string that has more than one character
+            var result = sprintingSlug.GetAdjective('o');
+            Assert.IsTrue(result is string);
+            Assert.IsFalse(string.IsNullOrEmpty(result));
+            Assert.IsTrue(result.Length > 1);
+        }
+
+        [TestMethod]
+        public void GetAdjective_LeadingCharMatches()
+        {
+            // Leading char matches given parameter (in uppercase)
+            Assert.AreEqual('A', sprintingSlug.GetAdjective('a').ToCharArray()[0]);
+            Assert.AreEqual('A', sprintingSlug.GetAdjective('A').ToCharArray()[0]);
+
+            Assert.AreEqual('H', sprintingSlug.GetAdjective('H').ToCharArray()[0]);
+            Assert.AreEqual('N', sprintingSlug.GetAdjective('n').ToCharArray()[0]);
+
+            Assert.AreEqual('Z', sprintingSlug.GetAdjective('z').ToCharArray()[0]);
+            Assert.AreEqual('Z', sprintingSlug.GetAdjective('Z').ToCharArray()[0]);
         }
 
         [TestMethod]
         public void GetAnimal()
         {
             // Returns an animal
-            // Trailing char matches given parameter
+            // Leading char matches given parameter (uppercase)
         }
 
-        // All trailing chars are uppercase
-        // Different chars  (a, h, n, w and z)
         // Unallowed input throws (' ', %, -, .)
         // Weird but allowed (Ó, ô, Ô, ö, Ö, ŏ, ō, Ō, ő, ø)
-        // Uppercase and lowercase parameters
+        // Nothing found
     }
 }
